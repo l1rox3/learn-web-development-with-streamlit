aceb51ede6f43863f4c8ab61963fb14d9aa622b8
Martin Krause <64521696+lerummi@users.noreply.github.com>
2024-06-26 19:23:42 +0000

    Updated main to show some nice stuff

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
 import streamlit as st
 
+st.title("Nice App")
+
 name = st.text_input("Insert your name here:")
 
-if st.button("Click me"):
+if name:
     st.write(f"Hello {name}!")
+
+st.text("Map illustration")
+
+#https://docs.streamlit.io/develop/api-reference/charts/st.map
+coords = [
+    {
+        "latitude": 51.221371, 
+        "longitude": 12.500010
+    }
+]
+st.map(data=coords)
