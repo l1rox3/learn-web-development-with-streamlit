fd1bcb38df2a9d9a5de8d25432da4a141aa18d18
Martin Krause <64521696+lerummi@users.noreply.github.com>
2025-05-14 13:48:08 +0000

    anpassung

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # learn-web-development-with-streamlit
 A project suitable for learn programming of a web app using streamline in Codespaces
+
+# blubb
+
--- app/main.py
+++ app/main.py
@@ -3,3 +3,25 @@ from links import FROH, TRAURIG
 
 # Emojis: 😎 💻 🚀 😢 😁
 # mehr Emojis gibt es hier: https://apps.timwhitlock.info/emoji/tables/unicode
+
+# Erstelle Eingabebox
+st.title("Willkommen zu meiner App! 😎")
+st.write(
+    "Hier kannst du deine Links zu den verschiedenen Seiten finden. "
+    "Klicke einfach auf die Links, um die Seiten zu besuchen. 💻"
+)
+selected = st.multiselect(
+    "Wähle die Links aus, die du besuchen möchtest:",
+    ["Froh", "Traurig"],
+    ["Froh", "Traurig"],
+)
+
+for select in selected:
+    if select == "Froh":
+        st.write("Hier ist der Link zu Froh: 😁")
+        st.markdown(FROH)
+    elif select == "Traurig":
+        st.write("Hier ist der Link zu Traurig: 😢")
+        st.markdown(TRAURIG)
+    else:
+        st.write("Bitte wähle einen Link aus.")
