e1e5cc77fe4f9f88f0076138226c299b990c5729
lirox <230372959+l1rox3@users.noreply.github.com>
2025-09-26 15:03:23 +0000

    quiz-rel

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import json
 import os
 import random
 from datetime import datetime
-from user_management import (
+from app.user_management import (
     authenticate_user,
     get_active_users,
     deactivate_user,
