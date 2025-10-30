22de94f29d16992ca9fe59e1ef782d2f2c9a4f13
lirox <230372959+l1rox3@users.noreply.github.com>
2025-10-30 07:17:44 +0000

    auto update

--- app/pages/quizzes.py
+++ app/pages/quizzes.py
@@ -11,7 +11,7 @@ import pandas as pd
 # Import der Auth-Funktionen
 import sys
 sys.path.append('.')
-from auth import AuthManager
+from pagesauth import AuthManager
 
 # Quiz Daten
 HINDUISMUS_QUIZ = {
